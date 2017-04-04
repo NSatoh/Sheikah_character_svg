f7299f0ff7e3edd5fc18ab006b2904441b642e7f
N.Satoh <nsatoh@azabu-jh.ed.jp>
2017-04-05 02:19:26 +0900

    Add sample character data.
      実験用データファイルを追加．動作自体はするが，
      まだ線を引く部分が一切実装されていない．
      点だけ打つ．

--- sheikah_characters_svg.py
+++ sheikah_characters_svg.py
@@ -175,14 +175,14 @@ start_style='l-corner'
 SVG_HEADER = r'''<?xml version="1.0" encoding="utf-8"?>
 <svg xmlns="http://www.w3.org/2000/svg"
      xmlns:xlink="http://www.w3.org/1999/xlink"
-     width="{size}px" height="{size}px" viewBox="{vmin} {vmin} {vmax} {vmax}">
+     width="{size}px" height="{size}px" viewBox="{x_min} {y_min} {size} {size}">
 
 '''
 
 
 class Char:
 
-    def __init__(self, char_name, polylines, dots):
+    def __init__(self, char_name, polylines, dots, test_print=None):
         '''
         :param str char_name:
         :param list[tuple[int]] polylines:
@@ -194,9 +194,12 @@ class Char:
         self.char_name = char_name
         self.polylines = polylines
         self.dots = dots
+        self.test_print=test_print
 
     def generate_svg(self, wide_size, narrow_size, line_width,
-                     size=1000, color='black', scale=1, grid_display=False):
+                     size=1000, color='black', scale=1, grid_display=False,
+                     line_join='bevel', stop_style=None, inner_corner_radius=None,
+                     dot_style=None):
         '''
         :param int wide_size:
         :param int narrow_size:
@@ -204,27 +207,89 @@ class Char:
         :rtype: str
         :return: SVG source of this character.
         '''
-        frame_size = size * scale
+        margin_width = (size - wide_size * 4 - narrow_size * 3)/2
         grid_margin = 0
         if grid_display:
-            grid_margin = 200*scale
-        viewbox_size = frame_size + grid_margin * 2
+            grid_margin = 200
+        viewbox_size = size + grid_margin * 2
 
-        svg_header = SVG_HEADER.format(size=viewbox_size,
-                                       vmin=-grid_margin, vmax=viewbox_size-grid_margin)
+        x_min = -grid_margin - margin_width
+        y_min = -grid_margin - margin_width - 4 * wide_size - 3 * narrow_size
+
+        svg_header = SVG_HEADER.format(size=viewbox_size * scale,
+                                       x_min=x_min * scale,
+                                       y_min=y_min * scale)
         svg_body = ''
         svg_footer = '\n</svg>'
+
+        for polyline in self.polylines:
+            path = polyline.generate_path(
+                      wide_size, narrow_size, line_width,
+                      color, scale,
+                      line_join, stop_style, inner_corner_radius)
+
+            svg_body += '\n' + path.output_svg()
+
+        if line_join == 'round':
+            style = 'round'
+        else:
+            style = 'square'
+        if dot_style:
+            # 指定があればそれを優先して，ここで強制上書き．
+            style = dot_style
+
+        for dot in self.dots:
+            path = dot.generate_path(
+                      wide_size, narrow_size, line_width,
+                      color, scale, style)
+            svg_body += '\n' + path.output_svg()
+
+
+        if grid_display:
+            ws = wide_size
+            ns = narrow_size
+
+            for i in range(8):
+                svg_body += '\n  <line x1="0" y1="{y}" x2="{x}" y2="{y}"'.format(
+                                    x=4*ws+3*ns, y=-((i+1)//2)*ws - (i//2)*ns)
+                svg_body += '\n     stroke="red" stroke-width="1" stroke-dasharray="20,5"/>'
+
+            for i in range(8):
+                svg_body += '\n  <line x1="{x}" y1="0" x2="{x}" y2="{y}"'.format(
+                                    y=-(4*ws+3*ns), x=((i+1)//2)*ws + (i//2)*ns)
+                svg_body += '\n     stroke="red" stroke-width="1" stroke-dasharray="20,5"/>'
+
+            svg_body += '\n  <line x1="0" y1="0" x2="0" y2="{y}"'.format(y=grid_margin)
+            svg_body += '\n     stroke="red" stroke-width="1"/>'
+
+            svg_body += '\n  <line x1="{x}" y1="0" x2="{x}" y2="{y}"'.format(x=ws,y=grid_margin)
+            svg_body += '\n     stroke="red" stroke-width="1"/>'
+
+            svg_body += '\n  <line x1="{x}" y1="0" x2="{x}" y2="{y}"'.format(x=ws+ns,y=grid_margin)
+            svg_body += '\n     stroke="red" stroke-width="1"/>'
+
+            svg_body += '\n  <text x="{x}" y="{y}" fill="black" text-anchor="middle" font-size="{fs}">'.format(
+                x=ws/2, y=grid_margin * 7/16, fs=grid_margin//8)
+            svg_body += '\n     {ws}</text>'.format(ws=ws)
+
+            svg_body += '\n  <text x="{x}" y="{y}" fill="black" text-anchor="middle" font-size="{fs}">'.format(
+                x=ws+ns/2, y=grid_margin * 7/16, fs=grid_margin//8)
+            svg_body += '\n     {ns}</text>'.format(ns=ns)
 
         return svg_header + svg_body + svg_footer
 
 
     def save_svg(self, wide_size, narrow_size, line_width,
-                 size=1000, color='black', scale=1, grid_display=False):
+                     size=1000, color='black', scale=1, grid_display=False,
+                     line_join='bevel', stop_style=None, inner_corner_radius=None,
+                     dot_style=None):
+
         '''
         テスト時のマニュアルセーブ用
         '''
         svg_output = self.generate_svg(wide_size, narrow_size, line_width,
-                 size, color, scale, grid_display)
+                 size, color, scale, grid_display, line_join, stop_style,
+                 inner_corner_radius, dot_style)
         fname = tkinter.filedialog.asksaveasfilename(
                     filetypes =[('svg files', '*.svg')])
         if fname:
@@ -233,7 +298,21 @@ class Char:
             f.close()
 
 
-class Polyline:
+class GryphElement:
+
+    def coordinate(self, cell_address, wide_size, narrow_size):
+        '''
+        セル番地から，セルの中心の座標を求める．
+        y座標は上が正．
+        '''
+        (i, j) = cell_address
+        x = (wide_size * (i+1) + narrow_size * i) / 2
+        y = (wide_size * (j+1) + narrow_size * j) / 2
+        return (x, y)
+
+
+
+class Polyline(GryphElement):
 
     def __init__(self, points, start_style, end_style):
         self.points = points
@@ -241,9 +320,8 @@ class Polyline:
         self.end_style = end_style
 
     def generate_path(self, wide_size, narrow_size, line_width,
-                      size=1000, color='black', scale=1,
-                      line_join='bevel', stop_style=None,
-                      inner_corner_radius=None):
+                      color='black', scale=1, line_join='bevel',
+                      stop_style=None, inner_corner_radius=None):
         '''
         :param int wide_size:
         :param int narrow_size:
@@ -259,10 +337,61 @@ class Polyline:
         backward_path = []
         # ここで色々処理
 
-        path = SvgPath(color)
+        path = SvgPath(color, scale)
         for point in forward_path:
             path.append(point)
         for point in backward_path:
+            path.append(point)
+
+        return path
+
+
+
+class Dot(GryphElement):
+
+    def __init__(self, i, j):
+        '''
+        (i, j): cell address
+        y座標は上が正．
+        '''
+        self.cell_address = (i, j)
+
+    def generate_path(self, wide_size, narrow_size, line_width,
+                      color='black', scale=1, style='square'):
+        '''
+        :param int wide_size:
+        :param int narrow_size:
+        :param int line_width:
+        :param str style: 'square' or 'round'
+        :rtype: str
+        :return: '<path d="M ...."/>'
+        '''
+
+        (center_x, center_y) = self.coordinate(self.cell_address,
+                                               wide_size, narrow_size)
+        forward_path = []
+
+        if style == 'square':
+            lw = line_width
+            forward_path += [Point(
+                                 coordinate=(center_x - lw/2, center_y - lw/2),
+                                 style='start'
+                            )]
+            forward_path += [Point(
+                                    coordinate=(center_x + lw/2, center_y - lw/2),
+                                    style='line'
+                            )]
+            forward_path += [Point(
+                                    coordinate=(center_x + lw/2, center_y + lw/2),
+                                    style='line'
+                            )]
+            forward_path += [Point(
+                                    coordinate=(center_x - lw/2, center_y + lw/2),
+                                    style='line'
+                            )]
+
+        path = SvgPath(color, scale)
+        for point in forward_path:
             path.append(point)
 
         return path
@@ -284,68 +413,83 @@ class Point:
 
 class SvgPath:
 
-    def __init__(self, color):
+    def __init__(self, color, scale):
         self.path_header = '  <path stroke="none" fill="{color}" d="'.format(color=color)
         self.path_body = ''
         self.path_footer = '\n    Z\n  "/>'
-
+        self.scale = scale
 
     def append(self, point):
 
-        if self.style == 'start':
+        if point.style == 'start':
             self.path_body += '\n    M'
-        elif self.style == 'line':
+        elif point.style == 'line':
             self.path_body += '\n    L' # HやVも使った方がいいんだろうか．
-        elif self.style == 'arc':
-            self.path_body += '\n    Q {x} {y}'.format(x=point.ctrl_pt[0], y=point.ctrl_pt[1])
+        elif point.style == 'arc':
+            self.path_body += '\n    Q {x} {y}'.format(
+                                   x=point.ctrl_pt[0] * self.scale,
+                                   y=-point.ctrl_pt[1] * self.scale)
 
-        self.path_body += ' {x} {y}'.format(x=self.x, y=self.y)
+        self.path_body += ' {x} {y}'.format(x=point.x * self.scale, y=-point.y * self.scale)
 
 
     def output_svg(self):
-        return self.svg_header + self.svg_body + self.svg_footer
+        return self.path_header + self.path_body + self.path_footer
 
 
 # ---------------------------------------------------------------------
 
 if __name__ == '__main__':
 
+    import test_char_data
+    alphabet_characters = test_char_data.characters
+    digit_characters = []
+
     wide_size = 180
     narrow_size = 45
     line_width_a = wide_size # alphabet
     line_width_d = narrow_size # digit
 
-    alphabet_characters = []
-    digit_characters = []
-
     for c in alphabet_characters:
         svg_output = c.generate_svg(wide_size, narrow_size, line_width_a)
-        f = open('test_{0}_{1}_{2}.svg'.format(
-                    c.char_name, wide_size, narrow_size),
-                'w')
+        fname = 'test_{0}_{1}_{2}.svg'.format(c.char_name, wide_size, narrow_size)
+        f = open(fname, 'w')
         f.write(svg_output)
         f.close()
+        print('write --> {}'.format(fname))
+        if c.test_print:
+            print(c.test_print)
 
         svg_output = c.generate_svg(wide_size, narrow_size, line_width_a,
                              color='cyan', grid_display=True)
-        f = open('test_{0}_{1}_{2}.svg'.format(
-                    c.char_name, wide_size, narrow_size),
-                'w')
+        fname = 'test_{0}_{1}_{2}_grid.svg'.format(c.char_name, wide_size, narrow_size)
+        f = open(fname, 'w')
         f.write(svg_output)
         f.close()
+        print('write --> {}'.format(fname))
+        if c.test_print:
+            print(c.test_print)
 
     for c in digit_characters:
         svg_output = c.generate_svg(wide_size, narrow_size, line_width_d)
-        f = open('test_{0}_{1}_{2}.svg'.format(
-                    c.char_name, wide_size, narrow_size),
-                'w')
+        fname = 'test_{0}_{1}_{2}.svg'.format(c.char_name, wide_size, narrow_size)
+        f = open(fname, 'w')
         f.write(svg_output)
         f.close()
+        print('write --> {}'.format(fname))
+        if c.test_print:
+            print(c.test_print)
 
         svg_output = c.generate_svg(wide_size, narrow_size, line_width_d,
                              color='cyan', grid_display=True)
-        f = open('test_{0}_{1}_{2}.svg'.format(
-                    c.char_name, wide_size, narrow_size),
-                'w')
+        fname = 'test_{0}_{1}_{2}_grid.svg'.format(c.char_name, wide_size, narrow_size)
+        f = open(fname, 'w')
         f.write(svg_output)
         f.close()
+        print('write --> {}'.format(fname))
+        if c.test_print:
+            print(c.test_print)
+
+    print('finish test')
+
+
